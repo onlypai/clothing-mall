b85de639d22657efd5c2061d20670dfa38d22f6a
onlypai <1184024857@qq.com>
2020-12-02 18:21:34 +0800

    懒加载gif图片更换

--- src/network/request.js
+++ src/network/request.js
@@ -3,9 +3,10 @@ import axios from 'axios'
 export function request(config) {
     //1、创建axios实例
     const instance = axios.create({
-        baseURL: 'http://123.207.32.32:8000',
-        timeout: 5000
-    })
+            baseURL: 'http://123.207.32.32:8000',
+            timeout: 7000
+        })
+        // 接口地址联系微信:coderwhy00
 
     //2.1、axios拦截器的使用-请求拦截 成功和失败
     instance.interceptors.request.use(config => {
